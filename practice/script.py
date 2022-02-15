09268c4f709f4315df9926e43af07da384d3d9e9
Pranav Marla <pranav.a.marla@gmail.com>
2022-02-15 17:05:57 -0500

    Write brute force solution with some minor optimizations
    Co-Authored-By: Nick Quinn <31631401+freakynoblegas@users.noreply.github.com>

--- practice/script.py
+++ practice/script.py
@@ -1,20 +1,170 @@
+#
+# Authors: Pranav Marla and Nick Quinn
+# Description: Program to solve Google's Hash Code 2022 'Pizza' practice problem: https://codingcompetitions.withgoogle.com/hashcode/round/00000000008f5ca9/00000000008f6f33#problem
+#
+# Currently, this brute force solution (containing some minor optimizations) is too slow to evaluate the last two test cases ('d' and 'e')
+
+from itertools import combinations
+from math import factorial, trunc
+from pathlib import Path
+from pprint import pprint
+import sys
+
+
 # Constants
 
-
+# Strategies:
+# Check if likes are larger than current combo
+# Check dislikes first, since if one dislike is there, than we can return immediately
+# For each iteration, check if it's possible to get a new best solution, otherwise return to save processing time
 
 # Functions
 
-def process_test_case(test_case_num):
+def evaluate_combo(combo, clients_preferences, total_num_clients, max_num_clients):
 
-    print(f'Case #{test_case_num}: YES')
+    num_clients = 0
+    error_margin = total_num_clients - max_num_clients
+    num_failures = 0
 
+    for potential_client in clients_preferences:
+
+        if num_failures >= error_margin:
+            #print(f'Num failures ({num_failures}) >= error margin ({error_margin}) -- aborting ...')
+            return num_clients
+
+        found_conflict = False
+
+        # Num combo ingredients should be >= num likes
+        if len(potential_client['likes']) > len(combo):
+            #print('Num combo ingredients should be >= num likes')
+            found_conflict = True
+            num_failures += 1
+            continue
+
+        # No dislikes present
+        for dislike in potential_client['dislikes']:
+            if dislike in combo:
+                found_conflict = True
+                num_failures += 1
+                #print('No dislikes should be present')
+                break
+
+        if found_conflict:
+            continue
+        
+        # All likes present
+        for like in potential_client['likes']:
+            if like not in combo:
+                found_conflict = True
+                num_failures += 1
+                #print('All likes should be present')
+                break
+
+        if not found_conflict:
+            #print('No conflicts')
+            num_clients += 1
+
+    return num_clients
+
+
+def process_test_case(total_num_clients, clients_preferences, ingredients, total_num_combos):
+
+    # Evaluate every combo
+    max_num_clients = 0
+    max_combo = []
+    
+    total_num_ingredients = len(ingredients)
+
+    # total_num_combos = 0
+    combo_counter = 0
+    # for num_choose in range(1, total_num_ingredients+1):
+    #     combos = list(combinations(ingredients, num_choose))
+    #     total_num_combos += len(combos)
+    # print(f'{total_num_combos=}')
+
+
+    # Generate and evaluate every combo
+    
+    for num_choose in range(1, total_num_ingredients+1):
+        combos = list(combinations(ingredients, num_choose))
+        # Eg. combo = ('pineapple', 'basil', 'cheese', 'peppers')
+        for combo in combos:
+            #print(f'{combo=}')
+
+            # Counter to help us keep track of how fast we're progressing
+            combo_counter += 1
+            if combo_counter % 1000 == 1:
+                print(f'Evaluating combo # {combo_counter}/{total_num_combos} ({trunc((combo_counter/total_num_combos)*100)}%)')
+            
+            num_clients = evaluate_combo(combo, clients_preferences, total_num_clients, max_num_clients)
+            #print(f'Num clients for this combo: {num_clients}\n')
+            if num_clients > max_num_clients:
+                max_num_clients = num_clients
+                max_combo = combo
+                #print(f'Max num clients increased to {max_num_clients}\n')
+                #print(f'New max combo: {max_combo}')
+                
+                if max_num_clients == total_num_clients:
+                    #print(f'Found the optimal combo -- stop looking\n')
+                    return max_combo, max_num_clients
+
+    return max_combo, max_num_clients
 
 # Execution
 
-num_test_cases = int(input())
+input_file = sys.argv[1]
+input_file_path = Path(input_file)
 
-for test_case in range(1, num_test_cases+1):
-    _ = int(input())
-    _ = [int(int_str) for int_str in input().split()]
-    _ = input()
-    process_test_case(test_case)
+# client_preferences = \
+# [
+#     {
+#         'likes': [],
+#         'dislikes': []
+#     },
+#     {
+#         'likes': [],
+#         'dislikes': []
+#     },
+# ]
+clients_preferences = []
+ingredients = set()
+
+with open(input_file, encoding='utf8') as f:
+    line = f.readline()
+    total_num_clients = int(line)
+    
+    for i in range(total_num_clients):
+        new_client = {'likes': [], 'dislikes': []}
+        new_client['likes'] = f.readline().split()[1:]
+        new_client['dislikes'] = f.readline().split()[1:]
+        clients_preferences.append(new_client)
+
+        ingredients.update(set(new_client['likes']), new_client['dislikes'])
+
+#pprint(f'{clients_preferences=}')
+print(f'{ingredients=}\n')
+
+# Number of combinations equals: n!/(n-r)!r!
+total_num_combos = 0
+num_of_ingredients = len(ingredients)
+for i in range(1, num_of_ingredients + 1):
+    total_num_combos += factorial(num_of_ingredients) / (factorial(num_of_ingredients - i) * factorial(i))
+print(f'{total_num_combos=}')
+
+max_combo, max_num_clients = process_test_case(total_num_clients, clients_preferences, ingredients, total_num_combos)
+max_combo = sorted(max_combo)
+output_elements = []
+output_elements.append(str(len(max_combo)))
+output_elements.extend(list(max_combo))
+output_str = ' '.join(output_elements)
+# print(output_str)
+print(f'{max_num_clients=}')
+print(f'{max_combo=}')
+
+# Write submission to file
+output_folder = Path('submissions')
+output_folder.mkdir(parents=True, exist_ok=True)
+output_file = output_folder/input_file_path.name
+
+with output_file.open('w+') as f:
+    f.write(output_str)
